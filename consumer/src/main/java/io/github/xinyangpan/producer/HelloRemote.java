b80a05fab9bd8fe38d4d794e537bff83a785dca2
Pan, Xinyang <seanpan2008@gmail.com>
2017-06-05 22:00:25 +0800

    consumer done

---
+++ consumer/src/main/java/io/github/xinyangpan/producer/HelloRemote.java
@@ -0,0 +1,13 @@
+package io.github.xinyangpan.producer;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "producer")
+public interface HelloRemote {
+
+	@RequestMapping(value = "/hello")
+	public String hello(@RequestParam(value = "name") String name);
+
+}
