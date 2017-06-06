f0f15e16b33ff7154927be4db44587a1a5aeb2c5
Pan, Xinyang <seanpan2008@gmail.com>
2017-06-06 16:56:05 +0800

    config in action

--- producer/src/main/java/io/github/xinyangpan/producer/HelloController.java
+++ producer/src/main/java/io/github/xinyangpan/producer/HelloController.java
@@ -1,15 +1,18 @@
 package io.github.xinyangpan.producer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	@Value("${neo.hello}")
+	private String message;
 
 	@RequestMapping("/hello")
 	public String index(@RequestParam String name) {
-		return "hello " + name + "，this is first messge";
+		return "hello " + name + "，" + message;
 	}
 
 }
